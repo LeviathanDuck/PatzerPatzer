6976d29c3c1038cf919e5465c4a8e9c4e89ef62d
Levi Allen <levi@leftcoastmediahouse.com>
2026-03-17 13:38:24 -0700

    Task 1.1 — setup build and hello world

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+// src/main.ts
+console.log("Patzer Pro");
+var app = document.getElementById("app");
+if (app) {
+  const h1 = document.createElement("h1");
+  h1.textContent = "Patzer Pro";
+  app.appendChild(h1);
+}
+//# sourceMappingURL=main.js.map
